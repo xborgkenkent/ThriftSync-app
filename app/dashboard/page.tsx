5406a67b9179898c2d7f06f2309a19380a4e449d
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-25 12:39:36 +0800

    Initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+import { Button } from "@/components/ui/button"
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,72 @@
+import { ArrowLeftRight , LayoutDashboard , HandCoins , Goal  , Settings, ChartColumnStacked  } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Transactions / Expenses",
+    url: "/transactions",
+    icon: ArrowLeftRight,
+  },
+  {
+    title: "Categories",
+    url: "/categories",
+    icon: ChartColumnStacked,
+  },
+  {
+    title: "Budgets",
+    url: "/budgets",
+    icon: HandCoins,
+  },
+  {
+    title: "Goals",
+    url: "/goals",
+    icon: Goal,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>TriftSync</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
