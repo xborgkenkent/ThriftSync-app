f0c84059f344acd0c5f6792dbee509acbab3624b
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-27 12:20:10 +0800

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <SidebarProvider>
           <AppSidebar />
-          <main>
+          <main className="w-full">
             <SidebarTrigger />
             {children}
           </main>
