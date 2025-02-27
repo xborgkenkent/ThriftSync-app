deb1068c3558f91918e06a3eef36d0df85313482
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-27 12:23:05 +0800

    Added top five expense component

---
+++ components/top-five.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { Pie, PieChart } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 90, fill: "var(--color-other)" },
+]
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Housing",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Food",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Transportation",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Healthcare",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig
+
+export default function TopFive() {
+  return (
+    <Card className="w-6/12 h-fit flex flex-col mt-3">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>Top 5 Expenses</CardTitle>
+        <CardDescription>January 2025</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px] pb-0 [&_.recharts-pie-label-text]:fill-foreground"
+        >
+          <PieChart>
+            <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+            <Pie data={chartData} dataKey="visitors" label nameKey="browser" />
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 font-medium leading-none">
+          Top five expenses this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing highest expenses this month
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
